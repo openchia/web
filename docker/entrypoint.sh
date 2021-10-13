27dafb333ad291a4580faf873c8e942388d77886
William Grzybowski <william@grzy.org>
2021-10-13 10:42:50 -0300

    LetsEncrypt changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,21 +15,26 @@ sed -i s/%%POOL_HOSTNAME%%/${POOL_HOSTNAME}/g /etc/nginx/sites-enabled/*
 
 certbot --agree-tos register --eff-email -m "${CERTBOT_EMAIL}"
 
-echo ${DOMAIN_LIST} | while read r; do
-	certpath="/etc/letsencrypt/live/${r}-0001"
-	mkdir -p ${certpath}
-	if [ ! -f "${certpath}/fullchain.pem" ]; then
-		openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout "${certpath}/privkey.pem" -out "${certpath}/fullchain.pem" -subj "/CN=localhost"
-		cp "${certpath}/fullchain.pem" "${certpath}/chain.pem"
-	fi
-	if [ ! -f "/etc/letsencrypt/dhparams.pem" ]; then
-		curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "/etc/letsencrypt/ssl-dhparams.pem"
-	fi
-done
+certpath="/etc/letsencrypt/live/openchia.io"
+mkdir -p ${certpath}
+if [ ! -f "${certpath}/fullchain.pem" ]; then
+	openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout "${certpath}/privkey.pem" -out "${certpath}/fullchain.pem" -subj "/CN=localhost"
+	cp "${certpath}/fullchain.pem" "${certpath}/chain.pem"
+fi
+if [ ! -f "/etc/letsencrypt/dhparams.pem" ]; then
+	curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "/etc/letsencrypt/ssl-dhparams.pem"
+fi
 
-echo "PATH=/bin:/sbin:/usr/sbin:/usr/bin" > /etc/cron.d/certbot-renew
-echo "@monthly certbot renew --nginx >> /var/log/cron.log 2>&1" >> /etc/cron.d/certbot-renew
+echo "PATH=/bin:/sbin:/usr/sbin:/usr/bin" > /etc/cron.d/certbot
+echo "0 */12 * * * root certbot renew --webroot -w /var/lib/letsencrypt/ >> /var/log/cron.log 2>&1" >> /etc/cron.d/certbot
+
+do_certonly() {
+	sleep 5
+	certbot certonly -n --webroot -w /var/lib/letsencrypt/ -d openchia.io,pool.openchia.io,www.openchia.io
+}
 
 cron
+
+do_certonly &
 
 exec nginx -g "daemon off;"
