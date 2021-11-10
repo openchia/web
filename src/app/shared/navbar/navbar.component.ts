7bf7be81f4ac228dbc59ca4cb77a3b1b1d77ee40
Djerfy <djerfy@gmail.com>
2021-11-10 21:45:22 +0100

    Add darkmode (step1)

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,20 @@
 import { LOCALE_ID, Component, Inject, OnInit } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Location, PopStateEvent } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
     public isCollapsed = true;
+    public isDarkEnabled = false;
     private lastPoppedUrl: string;
     private yScrollStack: number[] = [];
+    private presentTheme$ = new BehaviorSubject<string>('theme-light');
 
     constructor(public location: Location, private router: Router, @Inject(LOCALE_ID) public locale: string) {
     }
@@ -32,6 +36,10 @@ export class NavbarComponent implements OnInit {
         this.location.subscribe((ev: PopStateEvent) => {
             this.lastPoppedUrl = ev.url;
         });
+        const savedTheme = localStorage.getItem('theme');
+        if(savedTheme) {
+            this.presentTheme$.next(savedTheme);
+        }
     }
 
     isHome() {
@@ -43,5 +51,14 @@ export class NavbarComponent implements OnInit {
         else {
             return false;
         }
+    }
+
+    updateTheme() {
+        this.presentTheme$.value == 'theme-light'
+        ? this.presentTheme$.next('theme-dark')
+        : this.presentTheme$.next('theme-light');
+
+        localStorage.setItem('theme', this.presentTheme$.value);
+        this.isDarkEnabled = !this.isDarkEnabled;
     }
 }
