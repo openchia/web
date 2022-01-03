a81f2ef191977030456eba45a9a38db169b6c5ce
Djerfy <djerfy@gmail.com>
2022-01-03 18:48:07 +0100

    Review of calculator component

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -4,47 +4,39 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
 
 @Component({
-    selector: 'app-calculator',
-    templateUrl: './calculator.component.html',
-    styleUrls: ['./calculator.component.css']
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
 })
 
 export class CalculatorComponent implements OnInit {
 
-    xch_current_price: number = 0;
-    xch_tb_month: number = 0;
-    ranger: number = 0;
-    constructor(private dataService: DataService, private route: ActivatedRoute){
+  xch_current_price: number = 0;
+  xch_tb_month: number = 0;
+  calculator: number = 0;
 
-    }
+  constructor(private dataService: DataService, private route: ActivatedRoute){
+  }
 
-    ngOnInit(): void {
-        this.route.paramMap.subscribe(data => {
-          this.dataService.getStats().subscribe(data => {
-            this.xch_current_price = data['xch_current_price'];
-            this.xch_tb_month = data['xch_tb_month'];
-          })
-        });
-      }
-    
-      valueChanged(e) {
-    console.log('e', e);
-}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(data => {
+      this.dataService.getStats().subscribe(data => {
+        this.xch_current_price = data['xch_current_price'];
+        this.xch_tb_month = data['xch_tb_month'];
+      })
+    });
+  }
 
-ConvertPlotsToSpace(value) {
-    return (value * 101.4 / 1024 );
-}
+  convertPlotsToSpace(value) {
+    return value * 101.4 / 1024;
+  }
 
-estimate_day(value) {
-    var tib = this.ConvertPlotsToSpace(value);
-    var cal = this.xch_tb_month * this.ConvertPlotsToSpace(value);
-    return cal;
-}
+  estimateDayXCH(value) {
+    return this.xch_tb_month * this.convertPlotsToSpace(value);
+  }
 
-estimateXCHtoUSD(value) {
-    var tib = this.ConvertPlotsToSpace(value);
-    var cal = this.xch_tb_month * this.ConvertPlotsToSpace(value);
-    return cal * this.xch_current_price['usd'];
-}
+  estimateDayUSD(value) {
+    return (this.xch_tb_month * this.convertPlotsToSpace(value)) * this.xch_current_price['usd'];
+  }
 
 }
