503c009f2eb02098cc9b1481d6497d1d1cc344b0
Djerfy <djerfy@gmail.com>
2022-02-04 21:11:21 +0100

    Move pool space chart in stats page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
 
 @Component({
     selector: 'app-landing',
@@ -9,41 +8,8 @@ import { DataService } from '../data.service';
 
 export class LandingComponent implements OnInit {
 
-  // options
-  legend: boolean = false;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = false;
-  yAxis: boolean = true;
-  showXAxisLabel: boolean = false;
-  showYAxisLabel: boolean = true;
-  yAxisLabel: string = 'Pool Space';
-  timeline: boolean = false;
-  data: any[] = null;
+  constructor() { }
 
-  colorScheme = {
-    domain: ['#129b00']
-  };
-
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-    this.dataService.getPoolSpace().subscribe((d) => {
-      this.data = [{
-        "name": "Size",
-        "series": (<any[]>d).map((item) => {
-          return ({
-            "name": (new Date(item['date']).toLocaleString()),
-            "value": item['size'],
-            "label": (item['size'] / 1024 / 1024 / 1024 / 1024 / 1024).toFixed(2).toString() + ' PiB',
-          })
-        })
-      }];
-    });
-  }
-
-  yAxisFormat(data) {
-    return (data / 1024 / 1024 / 1024 / 1024 / 1024).toFixed(2).toString() + ' PiB';
-  }
+  ngOnInit() { }
 
 }
