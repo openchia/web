1cdd650175239167c3551fe93814e0dee5071c26
William Grzybowski <william@grzy.org>
2022-02-09 15:45:11 -0300

    Add tls env var config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,12 @@ if [ -z "${CERTBOT_EMAIL}" ]; then
 	exit 1
 fi
 
+if [ -n "${TLS_CERT}" -a -n "${TLS_KEY}" ]; then
+	sed -i "s,%%TLS%%,tls ${TLS_CERT} ${TLS_KEY},g" /etc/Caddyfile
+else
+	sed -i s,%%TLS%%,,g /etc/Caddyfile
+fi
+
 sed -i s,%%WWW_DOMAIN%%,${WWW_DOMAIN},g /etc/Caddyfile
 sed -i s,%%POOL_DOMAIN%%,${POOL_DOMAIN},g /etc/Caddyfile
 sed -i s,%%REDIRECT_DOMAIN%%,${REDIRECT_DOMAIN},g /etc/Caddyfile
