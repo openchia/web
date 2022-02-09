2d0ab21df3dcc51d836baf6c1370976c3cadf328
William Grzybowski <william@grzy.org>
2022-02-09 16:41:02 -0300

    Add defaults

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,10 +12,11 @@ else
 	sed -i s,%%TLS%%,,g /etc/Caddyfile
 fi
 
-sed -i s,%%WWW_DOMAIN%%,${WWW_DOMAIN},g /etc/Caddyfile
-sed -i s,%%POOL_DOMAIN%%,${POOL_DOMAIN},g /etc/Caddyfile
-sed -i s,%%REDIRECT_DOMAIN%%,${REDIRECT_DOMAIN},g /etc/Caddyfile
-sed -i s,%%API_HOSTNAME%%,${API_HOSTNAME},g /etc/Caddyfile
-sed -i s,%%POOL_HOSTNAME%%,${POOL_HOSTNAME},g /etc/Caddyfile
+sed -i s,%%EMAIL%%,${CERTBOT_EMAIL},g /etc/Caddyfile
+sed -i s,%%WWW_DOMAIN%%,${WWW_DOMAIN:=localhost},g /etc/Caddyfile
+sed -i s,%%POOL_DOMAIN%%,${POOL_DOMAIN:=pool},g /etc/Caddyfile
+sed -i s,%%REDIRECT_DOMAIN%%,${REDIRECT_DOMAIN:=www},g /etc/Caddyfile
+sed -i s,%%API_HOSTNAME%%,${API_HOSTNAME:=api},g /etc/Caddyfile
+sed -i s,%%POOL_HOSTNAME%%,${POOL_HOSTNAME:=pool},g /etc/Caddyfile
 
 exec caddy run --config /etc/Caddyfile
