a185c4f2255cb09b3faf90fac90bb3e100f944a6
Djerfy <djerfy@gmail.com>
2022-02-11 19:26:07 +0100

    Fix logformat variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@ set -x
 
 sed -i "s,%%DOMAIN%%,${DOMAIN:=localhost},g" /etc/Caddyfile
 sed -i "s,%%LOGLEVEL%%,${LOGLEVEL:=INFO},g" /etc/Caddyfile
-sed -i "s,%%LOGFORMAT%${LOGFORMAT:=json}%,,g" /etc/Caddyfile
+sed -i "s,%%LOGFORMAT%%,${LOGFORMAT:=json},g" /etc/Caddyfile
 
 exec caddy run --config /etc/Caddyfile
