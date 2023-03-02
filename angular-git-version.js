af4c9127479fcaa3e688e390ca05a78ade2aedad
Djerfy <djerfy@gmail.com>
2023-03-02 20:18:52 +0100

    misc: improve github details in footer
    Signed-off-by: Djerfy <djerfy@gmail.com>

--- angular-git-version.js
+++ angular-git-version.js
@@ -1,6 +1,12 @@
 const git = require('git-rev-sync');
 const { writeFileSync } = require('fs');
-const gitInfo = { commitShort: git.short(), commitLong: git.long(), branchName: git.branch() };
+const gitInfo = {
+    commitShort: git.short(),
+    commitLong: git.long(),
+    branchName: git.branch(),
+    tagName: git.tag(),
+    date: git.date()
+};
 const ts = 'export const gitVersion = ' + JSON.stringify(gitInfo, null, 2);
 
 writeFileSync('./src/environments/git-version.ts', ts);
