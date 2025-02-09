61045a1aa3b6ca2c2c9424b7e61bf91ba11f5721
djerfy <djerfy@gmail.com>
2025-02-09 14:05:08 +0100

    feat: upgrade angular dependencies
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/environments/git-version.ts
+++ src/environments/git-version.ts
@@ -1,7 +1,7 @@
 export const gitVersion = {
-  "commitShort": "3b09e8b",
-  "commitLong": "3b09e8bc62de71be5e378ff6f2066902cde1841d",
+  "commitShort": "2e843a9",
+  "commitLong": "2e843a94f315264455afcd20ca9df19e9cf39449",
   "branchName": "feat/upgrade-angular-18",
   "tagName": "None",
-  "date": "2025-02-08T18:51:54.000Z"
+  "date": "2025-02-09T12:59:56.000Z"
 }
