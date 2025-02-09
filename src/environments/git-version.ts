348f7db992d24bfa0edce72da6a9de7117041e1f
djerfy <djerfy@gmail.com>
2025-02-09 14:05:53 +0100

    fix: reset git-version file
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/environments/git-version.ts
+++ src/environments/git-version.ts
@@ -1,7 +1,7 @@
 export const gitVersion = {
-  "commitShort": "2e843a9",
-  "commitLong": "2e843a94f315264455afcd20ca9df19e9cf39449",
-  "branchName": "feat/upgrade-angular-18",
-  "tagName": "None",
-  "date": "2025-02-09T12:59:56.000Z"
+  "commitShort": "",
+  "commitLong": "",
+  "branchName": "",
+  "tagName": "",
+  "date": ""
 }
